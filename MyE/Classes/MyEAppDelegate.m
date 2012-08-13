e0316c7324cbfa9cf635fe371e867b51aaf66177
Ye Yuan <snyuan@macmini.local>
2012-08-13 16:18:05 -0700

    前一个版本里面有问题，Dr.Braun报告说有可能会Crash，主要是iPhone点击home键退出到主屏幕，再次点击进入MyE程序时，可能导致crash。经过检查，发现程序进入前台时，要调用当前面板的downloadModelFromServer函数，但在login面板是，获取当前的vc后，也调用了downloadModelFromServer函数，但login面板没有这个函数，导致程序crash。这里先修正了这个问题。但Dr.Braun报告的在Schedule面板退出由进入时导致crash的问题还没找到。

--- MyE/Classes/MyEAppDelegate.m
+++ MyE/Classes/MyEAppDelegate.m
@@ -25,6 +25,23 @@
 {
     // Override point for customization after application launch.
     sleep(2);//让程序休眠两秒，以使Launch image多停留一会。
+    
+    //想显示记录下来的被迫退出的原因，仅用于调试
+//    NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
+//    
+//    NSInteger exitcode = [prefs integerForKey:@"exitcode"];
+//    
+//    if (exitcode < 0) {
+//        NSLog(@"exit by applicationWillTerminate, abnormally.");
+//        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Warning" 
+//                                                        message:@"App exit with exit code of -1 last time."
+//                                                       delegate:self 
+//                                              cancelButtonTitle:@"Close" 
+//                                              otherButtonTitles: nil];   
+//        [alert show];
+//        [prefs setInteger:0  forKey:@"exitcode"]; //恢复此exitcode为0的状态
+//    }
+    
     return YES;
 }
 							
@@ -42,6 +59,7 @@
      Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
      If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
      */
+
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
@@ -52,32 +70,44 @@
     UINavigationController *nc = (UINavigationController *)self.window.rootViewController;
     NSArray *subVS = [nc viewControllers];
     if([subVS count] == 1){// 在HouseListView模块时，view堆栈的数目是1
-        MyEHouseListViewController * hlvc= [subVS objectAtIndex:0];
-        [hlvc downloadModelFromServer];
+        id vc = [subVS objectAtIndex:0];
+        
+        // view堆栈的数目是1时，还有可能是在MyELoginViewController面板，所以这里必须保证此vc却是是MyEHouseListViewController
+        if ([vc isKindOfClass:[MyEHouseListViewController class]]) {
+            MyEHouseListViewController * hlvc= vc;
+            [hlvc downloadModelFromServer];
+        }
+        
     }
     else if([subVS count] == 2){// 在进入TabBarController的任何一个直接子view时，view堆栈的数目是2
-        MyEMainTabBarController * mtbc= [subVS objectAtIndex:1];
-        id selectedVC = [mtbc selectedViewController];
+        id vc= [subVS objectAtIndex:1];
+        if ([vc isKindOfClass:[MyEMainTabBarController class]]) {
+            MyEMainTabBarController * mtbc = vc;
+            id selectedVC = [mtbc selectedViewController];
+            
+            if([selectedVC isKindOfClass:[MyEDashboardViewController class]]){
+                [(MyEDashboardViewController *)selectedVC downloadModelFromServer];
+            }
+            if([selectedVC isKindOfClass:[MyEScheduleViewController class]]){
+                MyEScheduleViewController *svc = (MyEScheduleViewController *)selectedVC;
+                if (svc.currentPanelType == SCHEDULE_TYPE_TODAY && svc.todayBaseViewController != nil)
+                    [svc.todayBaseViewController downloadModelFromServer];
+                else if (svc.currentPanelType == SCHEDULE_TYPE_WEEKLY && svc.weeklyBaseViewController != nil)
+                    [svc.weeklyBaseViewController downloadModelFromServer];
+                else if (svc.currentPanelType == SCHEDULE_TYPE_NEXT24HRS && svc.next24HrsBaseViewController != nil)
+                    [svc.next24HrsBaseViewController downloadModelFromServer];
+            }
+            if([selectedVC isKindOfClass:[MyEVacationMasterViewController class]]){
+                [(MyEVacationMasterViewController *)selectedVC downloadModelFromServer];
+            }
+            if([selectedVC isKindOfClass:[MyESettingsViewController class]]){
+                [(MyESettingsViewController *)selectedVC downloadModelFromServer];
+            }
+        } else {//有可能是view堆栈的最顶上vc不是MyEMainTabBarController，
+            // 这种情况不应该发生，但有可能是程序设计时没有想到而导致的未处理的情况。所以这里进行调试检查一下。
+            NSLog(@"This message indicates that the top vc in vc stack is no the  MyEMainTabBarController");
+        }
         
-        if([selectedVC isKindOfClass:[MyEDashboardViewController class]]){
-            [(MyEDashboardViewController *)selectedVC downloadModelFromServer];
-        }
-        if([selectedVC isKindOfClass:[MyEScheduleViewController class]]){
-            NSLog(@"Schedule");
-            MyEScheduleViewController *svc = (MyEScheduleViewController *)selectedVC;
-            if (svc.currentPanelType == SCHEDULE_TYPE_TODAY)
-                [svc.todayBaseViewController downloadModelFromServer];
-            else if (svc.currentPanelType == SCHEDULE_TYPE_WEEKLY)
-                [svc.weeklyBaseViewController downloadModelFromServer];
-            else if (svc.currentPanelType == SCHEDULE_TYPE_NEXT24HRS)
-                [svc.next24HrsBaseViewController downloadModelFromServer];
-        }
-        if([selectedVC isKindOfClass:[MyEVacationMasterViewController class]]){
-            [(MyEVacationMasterViewController *)selectedVC downloadModelFromServer];
-        }
-        if([selectedVC isKindOfClass:[MyESettingsViewController class]]){
-            [(MyESettingsViewController *)selectedVC downloadModelFromServer];
-        }
     }else if([subVS count] > 2){// 在vacation模块的添加、修改vacation时，view堆栈的数目是3或4
         NSLog(@"%i", [subVS count]);
     }
@@ -89,6 +119,7 @@
      Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
      */
     NSLog(@"----------applicationDidBecomeActive");
+
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
@@ -98,6 +129,12 @@
      Save data if appropriate.
      See also applicationDidEnterBackground:.
      */
+    //想记录下来被迫退出的原因，仅用于调试
+//    NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
+//
+//    [prefs setInteger:-1  forKey:@"exitcode"]; 
+//
+//    [prefs synchronize];
 }
 
 @end
