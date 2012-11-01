2684a147a7d87503c5fc2211fe2f4629510baa0a
Haifeng Guo <hai2feng@gmail.com>
2012-10-31 22:55:27 -0500

    Revert "Revert "login page / title of tab view / enter the same tab view when back from the house list""
    This reverts commit cc7232f4a676b761ac1ed798373839adfd55f252.

--- MyE/Classes/Shcedule/MyEScheduleViewController.m
+++ MyE/Classes/Shcedule/MyEScheduleViewController.m
@@ -50,7 +50,8 @@
 	// Do any additional setup after loading the view, typically from a nib.
     self.parentViewController.navigationItem.rightBarButtonItem.target = self;
     self.parentViewController.navigationItem.rightBarButtonItem.action = @selector(refreshAction);
-    self.parentViewController.title = @"Schedule";
+//    self.parentViewController.title = @"Schedule";
+    
     
     [self _createNext24HrsViewControllerIfNescessary];
     self.next24HrsBaseViewController.view.hidden = false;
