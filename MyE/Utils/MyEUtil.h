34064acef510e60e4dda294cb0847d91603b39ff
翟强 <Me@tekidzuyotekiMac-mini.local>
2014-05-10 14:56:31 +0800

    1.修改了device的数据模型

--- MyE/Utils/MyEUtil.h
+++ MyE/Utils/MyEUtil.h
@@ -29,8 +29,8 @@
 //#define URL_FOR_SETTINGS_SAVE @"http://www.myenergydomain.com/setting_save.do"
 
 #define GetServer [ServerUtils getServierIp]
-#define GetRequst(API) [@"http://www.myenergydomain.com:80" stringByAppendingFormat:@"%@",API]
-//#define GetRequst(API) [@"http://192.168.0.80:4000" stringByAppendingFormat:@"%@",API]
+//#define GetRequst(API) [@"http://www.myenergydomain.com:80" stringByAppendingFormat:@"%@",API]
+#define GetRequst(API) [@"http://192.168.0.80:4000" stringByAppendingFormat:@"%@",API]
 
 #define URL_FOR_LOGIN @"/user/userJson_login_phone.do"
 #define URL_FOR_HOUSELIST_VIEW @"/house_findHouseList.do"
