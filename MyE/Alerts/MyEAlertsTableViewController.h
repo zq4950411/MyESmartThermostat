5c24b0e058b80df576ad7ad89191f3189837c7ff
Ye Yuan <snyuan@gmail.com>
2014-06-04 23:11:22 -0700

    add pagination to Alerts panel

--- MyE/Alerts/MyEAlertsTableViewController.h
+++ MyE/Alerts/MyEAlertsTableViewController.h
@@ -7,13 +7,20 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "UITableView+DragLoad.h"
+typedef enum {
+    ALERT_LOAD_TYPE_INIT,
+    ALERT_LOAD_TYPE_PULL_REFRESH,
+    ALERT_LOAD_TYPE_DRAG_LOADMORE
+} ALERT_LOAD_TYPE;
 
-@interface MyEAlertsTableViewController : UITableViewController<MyEDataLoaderDelegate,MBProgressHUDDelegate, UIAlertViewDelegate,EGORefreshTableHeaderDelegate>
+@interface MyEAlertsTableViewController : UITableViewController
+<MyEDataLoaderDelegate,MBProgressHUDDelegate, UIAlertViewDelegate,UITableViewDragLoadDelegate>
 {
 @private
     MBProgressHUD *HUD;
-    EGORefreshTableHeaderView *_refreshHeaderView;
-    BOOL _isRefreshing;
+    NSUInteger _totalCount;//total alert count on server
+    NSInteger _pageIndex;//current zero-based page index that has already loaded, init value is -1 indicating not load any thing
 }
 @property (strong, nonatomic) NSMutableArray *alerts;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *sidebarButton;
